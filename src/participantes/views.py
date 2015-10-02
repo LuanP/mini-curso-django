8a808cbf01124e2c76ba43bd3f31fb2dc85a4472
Luan <luanpab@gmail.com>
2015-10-02 19:06:26 -0300

    Added pillow

--- src/participantes/views.py
+++ src/participantes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from participantes.models import Participante
 
+
 def lista_participantes(request):
     participantes = Participante.objects.all()
     return render(request, 'list.html', {'participantes': participantes})
